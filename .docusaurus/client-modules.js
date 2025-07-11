f4e6126d3ddb2400b18dd1e73c083608538182f6
witch-factory <soakdma37@gmail.com>
2025-07-11 23:22:29 +0900

    docusaurus 시절 파일 삭제

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/kimsunghyun/Desktop/projects/articles-archive/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/kimsunghyun/Desktop/projects/articles-archive/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/kimsunghyun/Desktop/projects/articles-archive/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/kimsunghyun/Desktop/projects/articles-archive/src/css/custom.css"),
-];
